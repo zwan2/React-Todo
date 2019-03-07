cb3bd008bcb67ca92a1d59aeb0d0db32162c19c8
zwan2 <2zwan@naver.com>
2019-03-07 16:44:10 +0900

    Updates

---
+++ precache-manifest.f7ee496408e5439fdb2952eba69de6f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "3f2b0e501712f4ae3a69",
+    "url": "/react-todo/static/js/main.3f2b0e50.chunk.js"
+  },
+  {
+    "revision": "9c92b932154c47279d8b",
+    "url": "/react-todo/static/js/2.9c92b932.chunk.js"
+  },
+  {
+    "revision": "3f2b0e501712f4ae3a69",
+    "url": "/react-todo/static/css/main.ab514a9e.chunk.css"
+  },
+  {
+    "revision": "d5586c73614235788509d026f7a6f43e",
+    "url": "/react-todo/index.html"
+  }
+];
